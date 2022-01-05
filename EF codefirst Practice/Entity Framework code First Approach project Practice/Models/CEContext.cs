d4eeeec037b43743b6a5176c74cf7fce3bd269e2
elhajuojy <souirimehdi311@gmail.com>
2022-01-05 12:57:54 +0100

    update databse

---
+++ EF codefirst Practice/Entity Framework code First Approach project Practice/Models/CEContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_code_First_Approach_project_Practice.Models
+{
+    public class CEContext:DbContext
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Company> Companys { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ Ef.cs
@@ -0,0 +1,12 @@
+public class Employee
+{
+    public int Employeeid { get; set; }
+
+    public string Name { get; set; }
+
+    public string Phone { get; set; }
+
+    public string Email { get; set; }
+
+    public int CompanyID { get; set; }
+}
